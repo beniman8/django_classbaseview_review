532f6746af7a5c62ee9b0683c2714cc4e5f230d1
beniman8 <benik@live.ca>
2021-03-28 19:40:32 -0400

    created the context

--- classbv/views.py
+++ classbv/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from classbv.models import Post
 
 
 class HomeTemplateView(TemplateView):
 
     template_name='classbv/index2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] =  Post.objects.get(id=1)
+        context["data"] = "This is data for the second page"
+        return context
+    
