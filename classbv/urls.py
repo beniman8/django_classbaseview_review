b60a5127db0b0d32aeb35a9879127e132bd8e41c
beniman8 <benik@live.ca>
2021-03-28 16:49:09 -0400

    subclassed the second generic view

--- classbv/urls.py
+++ classbv/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import HomeTemplateView
 
 
 app_name = 'website'
@@ -7,4 +8,5 @@ app_name = 'website'
 urlpatterns = [
     
     path('', TemplateView.as_view(template_name='classbv/index.html',extra_context={'title':'Custom Title'})),
+    path('home/',HomeTemplateView.as_view()),
 ]
