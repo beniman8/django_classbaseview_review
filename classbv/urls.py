707ca8242e5e2a070da01b37d076619489ad5bf9
beniman8 <benik@live.ca>
2021-03-28 20:31:05 -0400

    set up redierectview on urls file

--- classbv/urls.py
+++ classbv/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,RedirectView
 from .views import HomeTemplateView
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     
     path('', TemplateView.as_view(template_name='classbv/index.html',extra_context={'title':'Custom Title'})),
     path('home/',HomeTemplateView.as_view()),
+    path('yt/',RedirectView.as_view(url='https://www.youtube.com'),name='youtube')
 ]
