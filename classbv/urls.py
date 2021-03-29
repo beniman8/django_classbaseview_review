fd0816edea6a33c57f4dd9d6ca775b8430dcac20
beniman8 <benik@live.ca>
2021-03-28 20:42:48 -0400

    added the 2 urls

--- classbv/urls.py
+++ classbv/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     
     path('', TemplateView.as_view(template_name='classbv/index.html',extra_context={'title':'Custom Title'})),
     path('home/',HomeTemplateView.as_view()),
-    path('yt/',RedirectView.as_view(url='https://www.youtube.com'),name='youtube')
+    path('yt/',RedirectView.as_view(url='https://www.youtube.com'),name='youtube'),
+    path('rd/<int:pk>/',PostPreLoadTaskView.as_view(),name='redirect'),
+    path('sp/<int:pk>/',SinglePostView.as_view(),name='singlepost'),
 ]
